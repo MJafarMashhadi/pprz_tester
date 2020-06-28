26f7beac4ea60047af9ab22c0f62a5a0be7eaf30
Mohammad Jafar Mashhadi <raindigital2007@gmail.com>
2020-06-28 16:32:55 -0600

    Added typing and improved formatting

--- pprz_tester/ivy_subscribe.py
+++ pprz_tester/ivy_subscribe.py
@@ -1,14 +1,20 @@
-import sys
 import logging
+import sys
+from typing import List, Tuple
+
 sys.path.append("../pprzlink/lib/v1.0/python")
-
 import pprzlink.message as message
-
+from pprzlink.ivy import IvyMessagesInterface
 
 logger = logging.getLogger('pprz_tester')
 
+
 class IvySubscribe:
-    def __init__(self,  ivy_link, message_types, allow_direct_calls=True):
+    def __init__(self,
+                 ivy_link: IvyMessagesInterface,
+                 message_types: List[Tuple[str]],
+                 allow_direct_calls: bool = True
+                 ):
         self.ivy_link = ivy_link
         self.message_types = message_types
         self.allow_direct_calls = allow_direct_calls
@@ -17,14 +23,14 @@ class IvySubscribe:
     def __call__(self, f):
         for message_class, message_name in self.message_types:
             msg_type_obj = message.PprzMessage(
-                class_name=message_class, 
+                class_name=message_class,
                 msg=message_name
             )
             self.ivy_link.subscribe(f, msg_type_obj)
             logger.info(f"Function {f.__module__}.{f.__name__} is listening to {message_class}.{message_name}")
             self.bound = True
-        
-        def dummy(*__, **_): 
+
+        def dummy(*__, **_):
             # Moved the if inside the function so it can be restored later if unsubbed
             if self.allow_direct_calls or not self.bound:
                 return f(*__, **_)
@@ -32,5 +38,5 @@ class IvySubscribe:
                 raise RuntimeError(f"Calls to {f.__module__}.{f.__name__} are not allowed")
 
         return dummy
-    
+
     # TODO: a pythonic way to unsub
