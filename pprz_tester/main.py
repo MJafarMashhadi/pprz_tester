9ab411b730c9615800c6e648e989327ce31432f4
Mohammad Jafar Mashhadi <raindigital2007@gmail.com>
2020-07-09 01:12:10 -0600

    Updated main.py to use observer pattern

--- pprz_tester/main.py
+++ pprz_tester/main.py
@@ -24,13 +24,50 @@ ivy = pl.ivy.IvyMessagesInterface(
 aircraft_list: Dict[int, aircraft.Aircraft] = dict()
 
 
-def add_arcraft_if_new(ac_id, kwargs):
+class Observer:
+    def __init__(self, ac):
+        self.ac = ac
+
+    def __call__(self, property_name, old_value, new_value):
+        self.notify(property_name, old_value, new_value)
+
+    def notify(self, property_name, old_value, new_value):
+        raise NotImplementedError()
+
+
+class CurrentBlockChanged(Observer):
+    def notify(self, property_name, old_value, new_value):
+        logger.info(f'Aircraft {self.ac.id} in block {new_value}: {self.ac._find_block_name(new_value)}')
+
+
+class APModeChanged(Observer):
+    def notify(self, property_name, old_value, new_value):
+        logger.debug(f'{self.ac.id} changed mode to {new_value}')
+        if self.ac.ap_mode == 2:  # AUTO1 or AUTO2
+            print(f'{self.ac.id} Mode = AUTO2, ready')
+
+
+class CircleCountChanged(Observer):
+    def notify(self, property_name, old_value, new_value):
+        logger.debug(f'{self.ac.id} circle count changed to {new_value}')
+
+
+def add_aircraft_if_new(ac_id, kwargs):
     if ac_id not in aircraft_list:
-        aircraft_list[ac_id] = aircraft.Aircraft(
-            ivy_link=ivy,
-            ac_id=ac_id,
-            **kwargs
-        )
+        new_ac = create_aircraft(ac_id, kwargs)
+        aircraft_list[ac_id] = new_ac
+
+
+def create_aircraft(ac_id, kwargs):
+    new_ac = aircraft.Aircraft(
+        ivy_link=ivy,
+        ac_id=ac_id,
+        **kwargs
+    )
+    new_ac.observe('cur_block', CurrentBlockChanged(new_ac))
+    new_ac.observe('ap_mode', APModeChanged(new_ac))
+    new_ac.observe('circle_count', CircleCountChanged(new_ac))
+    return new_ac
 
 
 @IvySubscribe(ivy_link=ivy, message_types=[("ground", "NEW_AIRCRAFT")])
@@ -38,14 +75,14 @@ def new_aircraft_callback(ac_id, msg):
     logger.info(f"New aircraft message {ac_id}: {msg}")
     ac_id = int(ac_id)
     kwargs = dict(auto_request_config=True)
-    add_arcraft_if_new(ac_id, kwargs)
+    add_aircraft_if_new(ac_id, kwargs)
 
 
 def request_aircraft_list():
     def aircraft_list_callback(ac_id, msg):
         for ac_id in map(int, msg['ac_list']):
             kwargs = dict(auto_request_config=True)
-            add_arcraft_if_new(ac_id, kwargs)
+            add_aircraft_if_new(ac_id, kwargs)
 
     ivy.send_request(
         class_name="ground",
